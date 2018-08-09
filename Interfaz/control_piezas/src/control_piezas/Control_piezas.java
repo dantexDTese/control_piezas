bf8d8b869096379987cde13f58019a691920c731
dantexD <cesar_dantexD@hotmail.com>
2018-08-08 23:10:36 -0500

    interfaz1

--- Interfaz/control_piezas/src/control_piezas/Control_piezas.java
+++ Interfaz/control_piezas/src/control_piezas/Control_piezas.java
@@ -1,12 +1,14 @@
 
 package control_piezas;
 
+import View.Principal;
+
 
 public class Control_piezas {
 
 
     public static void main(String[] args) {
-
+           new Principal().setVisible(true);
     }
     
 }
